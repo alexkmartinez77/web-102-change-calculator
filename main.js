feae1f268004990216900c270e6ad84427c7feed
Alex Martinez <alexkmartinez77@gmail.com>
2021-09-26 17:30:35 -0700

    calculateChange function is running correctly for all denominations and coin change

--- main.js
+++ main.js
@@ -2,17 +2,54 @@
 function calculateChange(tendered, cost){
 
     var change = tendered - cost;
+    console.log(change);
 
     //Calculate quantity of $20 bills to return
     var twentyChange = Math.floor(change / 20);
-    var remainingChange = change % 20;
+    var remainingChange = (change % 20).toFixed(2);
     document.getElementById('twenty-dollars-output').innerHTML = twentyChange;
-    
 
     //Calculate quantity of $10 bills to return
-    //Calculate quantity of $50 bills to return
+    var tenChange = Math.floor(remainingChange / 10);
+    remainingChange = (remainingChange % 10).toFixed(2);
+    document.getElementById('ten-dollars-output').innerHTML = tenChange;
+
+    //Calculate quantity of $5 bills to return
+    var fiveChange = Math.floor(remainingChange / 5);
+    remainingChange = (remainingChange % 5).toFixed(2);
+    document.getElementById('five-dollars-output').innerHTML = fiveChange;
+    
     //Calculate quantity of $2 bills to return
+    var twoChange = Math.floor(remainingChange / 2);
+    remainingChange = (remainingChange % 2).toFixed(2);
+    document.getElementById('two-dollars-output').innerHTML = twoChange;
+
     //Calculate quantity of $1 bills to return
+    var oneChange = Math.floor(remainingChange / 1);
+    remainingChange = (remainingChange % 1).toFixed(2);
+    document.getElementById('dollars-output').innerHTML = oneChange;
+    
+    //Calculate quantity of quarters to return
+    var quarterChange = Math.floor(remainingChange / .25);
+    remainingChange = (remainingChange % .25).toFixed(2);
+    document.getElementById('quarters-output').innerHTML = quarterChange;
+    console.log('After Quarters ' + remainingChange);
+
+    //Calculate quantity of dimes bills to return
+    var dimeChange = Math.floor(remainingChange / .10);
+    remainingChange = (remainingChange % .10).toFixed(2);
+    document.getElementById('dimes-output').innerHTML = dimeChange;
+
+    //Calculate quantity of nickels bills to return
+    var nickelChange = Math.floor(remainingChange / .05);
+    remainingChange = (remainingChange % .05).toFixed(2);
+    document.getElementById('nickels-output').innerHTML = nickelChange;
+
+    //Calculate quantity of pennies bills to return
+    var pennyChange = Math.floor(remainingChange / .01);
+    remainingChange = (remainingChange % .01).toFixed(2);
+    document.getElementById('pennies-output').innerHTML = pennyChange;
+    console.log(remainingChange);
 }
 
 // handleClickEvent function
