753f05fc216f1a06f452e0e1df2167e84cb8e1ad
Alex Martinez <alexkmartinez77@gmail.com>
2021-11-04 11:36:39 -0700

    Adds express as a dependency.

--- server/index.js
+++ server/index.js
@@ -1,5 +1,3 @@
-const app = require("./server");
-
+const server = require('./server');
 const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
+server.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
